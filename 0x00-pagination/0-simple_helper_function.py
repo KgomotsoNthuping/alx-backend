557ee8571de2be81aa482c50a45c39d903b1a986
Kgomotso Nthuping <138695830+KgomotsoNthuping@users.noreply.github.com>
2024-06-23 21:39:50 +0200

    page

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" module """
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Retrieve index range """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return start, end
