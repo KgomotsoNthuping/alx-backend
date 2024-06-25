9daa53a953c018c8e72725a895dd3e96f57f1752
Kgomotso Nthuping <138695830+KgomotsoNthuping@users.noreply.github.com>
2024-06-25 13:50:08 +0200

    cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""cahce"""
+
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """class LRUCache"""
+
+    def __init__(self):
+        """initialise cache"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add item"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get item"""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
