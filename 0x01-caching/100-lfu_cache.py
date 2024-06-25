403f280f86a0249f07d1ba5414c6d861f1850a53
Kgomotso Nthuping <138695830+KgomotsoNthuping@users.noreply.github.com>
2024-06-25 13:53:47 +0200

    cache

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""cache"""
+from threading import RLock
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """Get items"""
+    def __init__(self):
+        """Initialise cache"""
+        super().__init__()
+        self.__stats = {}
+        self.__rlock = RLock()
+
+    def put(self, key, item):
+        """Add item"""
+        if key is not None and item is not None:
+            keyOut = self._balance(key)
+            with self.__rlock:
+                self.cache_data.update({key: item})
+            if keyOut is not None:
+                print('DISCARD: {}'.format(keyOut))
+
+    def get(self, key):
+        """Get item"""
+        with self.__rlock:
+            value = self.cache_data.get(key, None)
+            if key in self.__stats:
+                self.__stats[key] += 1
+        return value
+
+    def _balance(self, keyIn):
+        """Remove earliest item"""
+        keyOut = None
+        with self.__rlock:
+            if keyIn not in self.__stats:
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    keyOut = min(self.__stats, key=self.__stats.get)
+                    self.cache_data.pop(keyOut)
+                    self.__stats.pop(keyOut)
+            self.__stats[keyIn] = self.__stats.get(keyIn, 0) + 1
+        return keyOut
